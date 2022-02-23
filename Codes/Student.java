ff68b9cd2083744137c4a77514e0502a7e661856
Ravindu Mihiranga <e18224@eng.pdn.ac.lk>
2022-02-23 16:02:50 +0530

    inserted getter methods to the student class

--- Codes/Student.java
+++ Codes/Student.java
@@ -2,10 +2,47 @@
 
 public class Student {
 
+    // non static attributes
+    String username;
+    String password;
     String firstName;
     String lastName;
     String faculty;
     int batchNo;
     int regNo;
-    
+
+    // Constructor for a new sign up
+    public Student(String username, String password, String firstName, String lastName, String faculty, int batchNo, int regNo){
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.faculty = faculty;
+        this.batchNo = batchNo;
+        this.regNo = regNo;
+    }
+
+    public String getUsername(){
+        return this.username;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+
+    public String getFirstName(){
+        return this.firstName;
+    }
+
+    public String getFaculty(){
+        return this.faculty;
+    }
+
+    public int getBatchNo(){
+        return this.batchNo;
+    }
+
+    public int getRegNo(){
+        return this.regNo;
+    }
 }
