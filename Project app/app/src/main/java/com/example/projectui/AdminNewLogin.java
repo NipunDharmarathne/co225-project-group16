5b0442805ede3b61a9b0d24ccdfcf5a962fe98ce
Ravindu Mihiranga <e18224@eng.pdn.ac.lk>
2022-05-18 22:44:40 +0530

    admin login page added

--- Project app/app/src/main/java/com/example/projectui/AdminNewLogin.java
+++ Project app/app/src/main/java/com/example/projectui/AdminNewLogin.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class NewAdminLogin extends AppCompatActivity {
+public class AdminNewLogin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_admin_login);
+        setContentView(R.layout.activity_admin_new_login);
     }
 }
