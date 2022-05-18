688e5396bd928b8845c67b281d204e899a656680
Ravindu Mihiranga <e18224@eng.pdn.ac.lk>
2022-05-18 19:56:18 +0530

    new user login added

--- app/src/main/java/com/example/projectui/NewUserLogin.java
+++ app/src/main/java/com/example/projectui/NewUserLogin.java
@@ -2,13 +2,47 @@ package com.example.projectui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NewUserLogin extends AppCompatActivity {
+
+    private Button back;
+    private Button next;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user_login);
+
+        back = (Button) findViewById(R.id.backBtn);
+        next = (Button) findViewById(R.id.nxtBtn);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                backPage();
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nextPage();
+            }
+        });
+    }
+
+    private void backPage(){
+        Intent intent = new Intent (NewUserLogin.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    private void nextPage(){
+        Intent intent = new Intent (NewUserLogin.this, Student.class);
+        startActivity(intent);
     }
 }
