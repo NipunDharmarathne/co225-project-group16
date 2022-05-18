6e4303b102af2771f2c6ca6b7a7cc9608baaecf0
Ravindu Mihiranga <e18224@eng.pdn.ac.lk>
2022-05-18 20:37:25 +0530

    added a seperate file

---
+++ app/src/main/java/com/example/projectui/NewAdminLogin.java
@@ -0,0 +1,14 @@
+package com.example.projectui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewAdminLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_admin_login);
+    }
+}
