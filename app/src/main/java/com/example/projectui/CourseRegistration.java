761038dfdf08bf3bac122d009030fe8044d93855
Ravindu Mihiranga <e18224@eng.pdn.ac.lk>
2022-05-18 16:14:40 +0530

    added the new LogIn

---
+++ app/src/main/java/com/example/projectui/CourseRegistration.java
@@ -0,0 +1,14 @@
+package com.example.projectui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CourseRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_registration);
+    }
+}
