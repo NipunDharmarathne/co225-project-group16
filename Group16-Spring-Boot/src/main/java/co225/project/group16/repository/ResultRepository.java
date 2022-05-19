cab58d84810a871281ef995ac1580fe23a3e4036
NipunDharmarathne <73479618+NipunDharmarathne@users.noreply.github.com>
2022-05-19 14:01:53 +0530

    Add files via upload

---
+++ Group16-Spring-Boot/src/main/java/co225/project/group16/repository/ResultRepository.java
@@ -0,0 +1,11 @@
+package co225.project.group16.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import co225.project.group16.model.Result;
+
+public interface ResultRepository extends JpaRepository<Result, Long>{
+
+}
