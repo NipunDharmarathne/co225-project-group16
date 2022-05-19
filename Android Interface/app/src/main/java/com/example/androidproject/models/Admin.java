9d9447d1e1a7da5efaa2f2c0f69c6eba2af26690
Ravindu Mihiranga <e18224@eng.pdn.ac.lk>
2022-05-20 00:26:49 +0530

    added the Admin class model

---
+++ Android Interface/app/src/main/java/com/example/androidproject/models/Admin.java
@@ -0,0 +1,140 @@
+package com.example.androidproject.models;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Admin {
+    @SerializedName("regNo")
+    private long regNo;
+    @SerializedName("username")
+    private String username;
+    @SerializedName("password")
+    private String password;
+    @SerializedName("fullName")
+    private String fullName;
+    @SerializedName("university")
+    private String university;
+    @SerializedName("faculty")
+    private String faculty;
+    @SerializedName("department")
+    private String department;
+    @SerializedName("year")
+    private int year;
+    @SerializedName("totalSubjectsInFirstSem")
+    private int totalSubjectsInFirstSem;
+    @SerializedName("totalSubjectsInSecondSem")
+    private int totalSubjectsInSecondSem;
+
+    public Admin() {
+    }
+
+    public Admin(long regNo, String username, String password, String fullName, String university, String faculty, String department, int year, int totalSubjectsInFirstSem, int totalSubjectsInSecondSem) {
+        this.regNo = regNo;
+        this.username = username;
+        this.password = password;
+        this.fullName = fullName;
+        this.university = university;
+        this.faculty = faculty;
+        this.department = department;
+        this.year = year;
+        this.totalSubjectsInFirstSem = totalSubjectsInFirstSem;
+        this.totalSubjectsInSecondSem = totalSubjectsInSecondSem;
+    }
+
+    public Admin(long regNo, String username, String fullName, String university, String faculty, String department, int year, int totalSubjectsInFirstSem, int totalSubjectsInSecondSem) {
+        this.regNo = regNo;
+        this.username = username;
+        this.fullName = fullName;
+        this.university = university;
+        this.faculty = faculty;
+        this.department = department;
+        this.year = year;
+        this.totalSubjectsInFirstSem = totalSubjectsInFirstSem;
+        this.totalSubjectsInSecondSem = totalSubjectsInSecondSem;
+    }
+
+    public long getRegNo() {
+        return regNo;
+    }
+
+    public void setRegNo(long regNo) {
+        this.regNo = regNo;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(String university) {
+        this.university = university;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getTotalSubjectsInFirstSem() {
+        return totalSubjectsInFirstSem;
+    }
+
+    public void setTotalSubjectsInFirstSem(int totalSubjectsInFirstSem) {
+        this.totalSubjectsInFirstSem = totalSubjectsInFirstSem;
+    }
+
+    public int getTotalSubjectsInSecondSem() {
+        return totalSubjectsInSecondSem;
+    }
+
+    public void setTotalSubjectsInSecondSem(int totalSubjectsInSecondSem) {
+        this.totalSubjectsInSecondSem = totalSubjectsInSecondSem;
+    }
+}
+
+
