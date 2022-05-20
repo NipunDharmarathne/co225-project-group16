b5292c7b69f78275f5b8d7d9f682a290e2045e28
Ravindu Mihiranga <e18224@eng.pdn.ac.lk>
2022-05-20 09:37:28 +0530

    Current GPA calc done

--- Android Interface/app/src/main/java/com/example/androidproject/StdResultsCalc.java
+++ Android Interface/app/src/main/java/com/example/androidproject/StdResultsCalc.java
@@ -2,13 +2,34 @@ package com.example.androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StdResultsCalc extends AppCompatActivity {
+
+    private Button CurrentGPA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_std_results_calc);
+
+        CurrentGPA = (Button) findViewById(R.id.button1);
+
+        CurrentGPA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCurrentGPA();
+            }
+        });
+
+
+    }
+
+    private void openCurrentGPA(){
+        Intent intent = new Intent(StdResultsCalc.this, com.example.androidproject.CurrentGPA.class);
+        startActivity(intent);
     }
 }
